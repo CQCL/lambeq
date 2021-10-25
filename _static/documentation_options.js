a051cd5ac3a4605e78036e0be531717103589305
dimkart <dimkart@users.noreply.github.com>
2021-10-25 15:09:51 +0000

    Update docs to output generated at 3feba14

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.3.dev4+gc7bccba',
+    VERSION: '0.1.3.dev5+g3feba14',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
