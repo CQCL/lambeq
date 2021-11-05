bc5c4c5f5443499bfd63879c75c61628a786e70c
y-richie-y <y-richie-y@users.noreply.github.com>
2021-11-05 15:31:40 +0000

    Update docs to output generated at e94064f

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.3.dev7+g4d89cae',
+    VERSION: '0.1.3.dev8+ge94064f',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
