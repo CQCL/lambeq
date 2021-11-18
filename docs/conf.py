daa7b0d580d80ba0e1c5f383a2099269d58a1c63
mayank-cse <72187020+mayank-cse@users.noreply.github.com>
2021-11-18 11:07:17 +0530

    removed the extra comment tags and spaces between words

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,5 @@
 # Configuration file for the Sphinx documentation builder.
-#
+
 # This file only contains a selection of the most common options. For a full
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
@@ -9,7 +9,7 @@
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#
+
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
@@ -63,9 +63,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # -- Options for HTML output -------------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
+# The theme to use for HTML and HTML Help pages. See the documentation for
 # a list of builtin themes.
-#
+
 html_theme = 'sphinx_rtd_theme'
 html_context = {
   'display_github': True,
