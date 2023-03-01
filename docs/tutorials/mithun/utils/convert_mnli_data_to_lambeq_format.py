0318d9bd2a0807c9eb933d2672c8347ff679b12a
Mitch Paul Mithun <mithunpaul08@gmail.com>
2023-03-01 13:05:24 -0800

    Read mnli into dict

--- docs/tutorials/mithun/utils/convert_mnli_data_to_lambeq_format.py
+++ docs/tutorials/mithun/utils/convert_mnli_data_to_lambeq_format.py
@@ -1,5 +1,11 @@
 import jsonlines
 from configs import *
+claim_ev_label = {}
+
 with jsonlines.open(MNLI_JSON_TRAIN) as f:
     for line in f.iter():
-        print(line)
+        key=(line["sentence1"], line["sentence2"])
+        claim_ev_label[key] = line["gold_label"]
+
+print(claim_ev_label)
+
