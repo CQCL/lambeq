51427e89ef94379ff8fb958bdf691e7284e87808
toumix <alexis@toumi.email>
2023-03-29 17:13:01 +0200

    update notebooks WIP

--- lambeq/pregroups/__init__.py
+++ lambeq/pregroups/__init__.py
@@ -16,6 +16,7 @@ __all__ = ['TextDiagramPrinter', 'diagram2str',
            'create_pregroup_diagram', 'is_pregroup_diagram', 'remove_cups',
            'remove_swaps', 'draw']
 
+
 from lambeq.pregroups.drawing import draw
 from lambeq.pregroups.text_printer import TextDiagramPrinter, diagram2str
 from lambeq.pregroups.utils import (create_pregroup_diagram,
