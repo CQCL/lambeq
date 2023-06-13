8370c21af99ef4a9e5ec9346b0e3c8fd9dea5b52
A.C.E07 <73689800+ACE07-Sev@users.noreply.github.com>
2023-06-13 22:04:56 +0800

    Addresses ImportError with test_with_pytest

--- tests/test_rewrite.py
+++ tests/test_rewrite.py
@@ -7,7 +7,7 @@ from lambeq import (AtomicType, Rewriter, CoordinationRewriteRule,
                     CurryRewriteRule, SimpleRewriteRule,
                     UNKRewriteRule)
 
-from lambeq.rewrite import HandleUnknownWords
+from lambeq.rewrite.base import HandleUnknownWords
 
 N = AtomicType.NOUN
 S = AtomicType.SENTENCE
