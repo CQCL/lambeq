8dcbba44e23f5417edd779cc7354b2033ce03d24
A.C.E07 <73689800+ACE07-Sev@users.noreply.github.com>
2023-06-13 13:35:00 +0800

    Fixes Flake8 error
    Fixes : lambeq/__init__.py:117:75: W291 trailing whitespace

--- lambeq/__init__.py
+++ lambeq/__init__.py
@@ -114,7 +114,7 @@ from lambeq.core.types import AtomicType
 from lambeq.pregroups import (create_pregroup_diagram, diagram2str,
                               is_pregroup_diagram, remove_cups, remove_swaps)
 from lambeq.rewrite import (RewriteRule, CoordinationRewriteRule,
-                            CurryRewriteRule, SimpleRewriteRule, Rewriter, 
+                            CurryRewriteRule, SimpleRewriteRule, Rewriter,
                             UNKRewriteRule)
 from lambeq.text2diagram import (
         CCGAtomicType, CCGRule, CCGRuleUseError, CCGTree,
