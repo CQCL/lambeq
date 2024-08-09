f585358b82bb5a3892d78ef134184e926353a570
nikhilkhatri <nikhilkhatri97@gmail.com>
2024-08-09 11:12:42 +0000

    Update docs to output generated at e012aa9

---
+++ _static/nav-config.js
@@ -0,0 +1,27 @@
+const navConfig = {
+    "navTextLinks": [
+
+        {
+            "title": "Getting started",
+            "href": "installation.html",
+        },
+        {
+            "title": "NLP 101",
+            "href": "nlp-intro.html",
+        },
+        {
+            "title": "Tutorials",
+            "href": "tutorials/sentence-input.html",
+        },
+        {
+            "title": "Code examples",
+            "href": "notebooks.html",
+        },
+        {
+            "title": "API reference",
+            "href": "root-api.html",
+        },
+    ],
+    "navProductName": "lambeq",
+    "navIconLinks": [],
+}
