0ea6bac2d78379df4917aae44543478088ab7702
Neil Ortega <neil.ortega@quantinuum.com>
2024-11-18 20:29:53 +0900

    Fix behavior of `Ty @ Diagram` when diagram is `Id` (#183)

--- tests/backend/test_grammar.py
+++ tests/backend/test_grammar.py
@@ -27,6 +27,7 @@ def test_Ty():
     assert a**1 == a
     assert a**2 == a @ a
     assert a**3 == a @ a @ a
+    assert c @ a == a @ c == a
 
     with raises(TypeError):
         a @ 'b'
@@ -151,7 +152,6 @@ def test_Spider():
 
 def test_Swap():
     a, b = map(Ty, 'ab')
-    ab = a @ b
 
     swap = Swap(a, b)
     assert swap.dagger().dagger() == swap
@@ -170,12 +170,16 @@ def test_Layer():
 
 def test_Id():
     a, b = map(Ty, 'ab')
+    c = Ty()
     box = Box('dimitri', a, b)
 
     assert box @ Id() == box.to_diagram()
     assert box @ Id(b) >> Id(b) @ box.dagger() == box @ box.dagger()
     assert Id().is_id == True
     assert Id().dagger() == Id()
+    assert Id(a) @ c == Id(a)
+    assert c @ Id(a) == Id(a)
+    assert a @ b @ Id(b) == Id(a @ b @ b)
 
 
 def test_Diagram():
